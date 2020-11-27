0378cb340296e638227d80e4432b9525a4a1d685
Hayin Ananta <72958200+hayinananta@users.noreply.github.com>
2020-11-27 20:33:59 +0700

    Add files via upload

---
+++ src/quiz/if1/pkg10119005/hayinananta/QUIZIF110119005HayinAnanta.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package quiz.if1.pkg10119005.hayinananta;
+
+/**
+ *
+ * @author Hayin
+ * NAMA     : Hayin Ananta
+ * KELAS    : IF-1
+ * NIM      : 10119005
+ * Deskripsi Program : Program ini menampilkan aktivitas seseorang dengan 
+ *                     berbasis Objek
+ */
+public class QUIZIF110119005HayinAnanta {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
