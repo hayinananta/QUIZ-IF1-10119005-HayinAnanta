70bb72a710491e9e36a5ce7102067a8cd1c4bb37
Hayin Ananta <72958200+hayinananta@users.noreply.github.com>
2020-11-27 21:47:00 +0700

    Add files via upload

---
+++ src/quiz/if1/pkg10119005/hayinananta/src/quiz/if1/pkg10119005/hayinananta/Customer.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package quiz.if1.pkg10119005.hayinananta;
+
+/**
+ *
+ * @author Hayin
+ * NAMA     : Hayin Ananta
+ * KELAS    : IF-1
+ * NIM      : 10119005
+ * Deskripsi Program : Program ini menampilkan program kasir dengan berbasis
+ *                     Objek
+ */
+public class Customer extends ServicePrice implements CustomerInvoice{
+    private String name, email;
+    private boolean member;
+    
+    public String getName() {
+        return this.name;
+    }
+    
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    public String getEmail() {
+        return this.email;
+    }
+    
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
+    public boolean isMember() {
+        if(super.checkMemberStatus(member)) {
+            
+        }
+    }
+    
+    public void setMember(boolean member) {
+        this.member = member;
+    }
+    
+    @Override
+    public String currentTime() {
+         //  object tanggal
+       Date date = new Date();
+        
+       // menampilkan waktu dan tanggal menggunakan toString()
+       System.out.println(date.toString());
+    }
+}
---
+++ src/quiz/if1/pkg10119005/hayinananta/src/quiz/if1/pkg10119005/hayinananta/QUIZIF110119005HayinAnanta.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package quiz.if1.pkg10119005.hayinananta;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Hayin
+ */
+public class QUIZIF110119005HayinAnanta {
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Customer customer = new Customer();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("===Program Kasir Rock n Roll Haircut===");
+        System.out.print("Customer Name : ");
+        customer.setName(scanner.next());
+        System.out.print("Customer Email : ");
+        customer.setEmail(scanner.next());
+    }
+    
+}
